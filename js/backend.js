83300357096ecf95aca10c57acebf7ba63b96d31
Irina14 <ira1786@bk.ru>
2018-12-28 13:07:08 +0700

    Внесены изменения в backend.js

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,8 @@
 
 (function () {
   var TIME = 10000;
+  var STATUS = 'Cтатус ответа: ';
+
   var Code = {
     SUCCESS: 200,
     ERROR_BAD_REQUEST: 400,
@@ -9,27 +11,32 @@
     ERROR_SERVER: 500
   };
 
-  var load = function (successHandler, errorHandler) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+  var ErrorText = {
+    BAD_REQUEST: 'Неверный запрос',
+    SEARCH: 'Ничего не найдено',
+    SERVER: 'Внутренняя ошибка сервера',
+    CONNECTION: 'Произошла ошибка соединения',
+    TIMEOUT: 'Истекло время ожидания'
+  };
 
+  var setEvents = function (xhr, successHandler, errorHandler) {
     xhr.addEventListener('load', function () {
       var error;
       switch (xhr.status) {
         case Code.SUCCESS:
           successHandler(xhr.response);
           break;
-        case Code.ERROR_BAD_REQUEST_CODE:
-          error = 'Неверный запрос';
+        case Code.ERROR_BAD_REQUEST:
+          error = ErrorText.BAD_REQUEST;
           break;
         case Code.ERROR_NOT_FOUND:
-          error = 'Ничего не найдено';
+          error = ErrorText.SEARCH;
           break;
         case Code.ERROR_SERVER:
-          error = 'Внутренняя ошибка сервера';
+          error = ErrorText.SERVER;
           break;
         default:
-          error = 'Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText;
+          error = STATUS + xhr.status + ' ' + xhr.statusText;
       }
 
       if (error) {
@@ -38,15 +45,19 @@
     });
 
     xhr.addEventListener('error', function () {
-      errorHandler('Произошла ошибка соединения');
+      errorHandler(ErrorText.CONNECTION);
     });
 
     xhr.addEventListener('timeout', function () {
-      errorHandler('Истекло время ожидания');
+      errorHandler(ErrorText.TIMEOUT);
     });
+  };
 
+  var load = function (successHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    setEvents(xhr, successHandler, errorHandler);
     xhr.timeout = TIME;
-
     xhr.open('GET', 'https://js.dump.academy/keksobooking/data');
     xhr.send();
   };
@@ -54,38 +65,8 @@
   var upload = function (data, successHandler, errorHandler) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      var error;
-      switch (xhr.status) {
-        case Code.SUCCESS:
-          successHandler(xhr.response);
-          break;
-        case Code.ERROR_BAD_REQUEST_CODE:
-          error = 'Неверный запрос';
-          break;
-        case Code.ERROR_SERVER:
-          error = 'Внутренняя ошибка сервера';
-          break;
-        default:
-          error = 'Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText;
-      }
-
-      if (error) {
-        errorHandler(error);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      errorHandler('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      errorHandler('Истекло время ожидания');
-    });
-
+    setEvents(xhr, successHandler, errorHandler);
     xhr.timeout = TIME;
-
     xhr.open('POST', 'https://js.dump.academy/keksobooking');
     xhr.send(data);
   };
