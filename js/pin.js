01550154d2ff58d2d22599a016b77bc6c0d4fe98
Irina14 <ira1786@bk.ru>
2018-12-29 21:36:22 +0700

    Внесены изменения.

--- js/pin.js
+++ js/pin.js
@@ -4,15 +4,14 @@
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
   var IMAGE_AVATAR = 'img/avatars/default.png';
-  var PIXEL = 'px';
 
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
   var renderPin = function (ad) {
     var pinElement = pinTemplate.cloneNode(true);
     if (ad.offer) {
-      pinElement.style.left = ad.location.x - PIN_WIDTH / 2 + PIXEL;
-      pinElement.style.top = ad.location.y - PIN_HEIGHT + PIXEL;
+      pinElement.style.left = ad.location.x - PIN_WIDTH / 2 + 'px';
+      pinElement.style.top = ad.location.y - PIN_HEIGHT + 'px';
 
       if (ad.author.avatar !== '') {
         pinElement.querySelector('img').src = ad.author.avatar;
